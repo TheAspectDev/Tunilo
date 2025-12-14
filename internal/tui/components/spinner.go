865c81a2660ce695be551e79cb2003c5a03d1984
Aspect <theaspectdev@gmail.com>
2025-12-14 20:37:48 +0330

    chore: change helper value color server-TUI

--- internal/tui/components/spinner.go
+++ internal/tui/components/spinner.go
@@ -166,7 +166,7 @@ func (m model) Update(msg tea.Msg) (tea.Model, tea.Cmd) {
 
 func (m model) HelpNotes() string {
 	var keyNote = lipgloss.NewStyle().Foreground(lipgloss.Color("#338f8cff"))
-	var valNote = lipgloss.NewStyle().Foreground(lipgloss.Color("#000000e3"))
+	var valNote = lipgloss.NewStyle().Foreground(lipgloss.Color("#5f5f5fff"))
 
 	return (keyNote.Render("↑/k") + " 	" + valNote.Render("move up")) + "			" + (keyNote.Render("q/esc") + " " + valNote.Render("kill tunnel")) + "\n" +
 		(keyNote.Render("↓/j") + "     " + valNote.Render("move down")) + "\n" +
